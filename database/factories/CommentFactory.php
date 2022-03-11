125565abeae7c610f30e872341750a611ca60be6
ares <ares.su@habook.com.tw>
2022-03-11 09:52:08 +0800

    - Change commentable_type value

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Comment;
 use App\Models\Post;
+use App\Models\Video;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CommentFactory extends Factory
@@ -15,8 +16,8 @@ class CommentFactory extends Factory
         return [
             'body'             => $this->faker->sentence,
             'commentable_id'   => $this->faker->randomDigitNotNull,
-            'commentable_type' => Post::Class,
-//          'commentable_type' => Vide::Class,
+            'commentable_type' => Post::TABLE,
+//          'commentable_type' => Video::TABLE,
         ];
     }
 }
