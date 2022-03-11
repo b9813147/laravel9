b8514fa1ca30cb3fe3e47418319f1a56e2ea10eb
ares <ares.su@habook.com.tw>
2022-03-11 14:20:38 +0800

    + LoginHistoryEvent.php

---
+++ app/Events/LoginHistoryEvent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+
+class LoginHistoryEvent
+{
+    use Dispatchable;
+
+    public function __construct()
+    {
+        //
+    }
+}
