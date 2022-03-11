349a2b6a769ae086bd9cb7b54a20a7b713c77438
ares <ares.su@habook.com.tw>
2022-03-11 14:21:27 +0800

    + StoreUserLoginHistoryListener

---
+++ app/Listeners/StoreUserLoginHistoryListener.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Listeners;
+
+class StoreUserLoginHistoryListener
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle($event)
+    {
+
+    }
+}
